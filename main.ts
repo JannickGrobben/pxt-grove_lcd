2f73a57b76e248dea7adda8741cf18e48687e5fb
JannickGrobben <28728688+JannickGrobben@users.noreply.github.com>
2023-06-07 01:05:10 +0200

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,17 @@ namespace grove_lcd {
 
     const LCD_CLEARDISPLAY = 0x01
 
+    const LCD_RETURNHOME = 0x02
+    const LCD_ENTRYMODESET = 0x07
+    let LCD_DISPLAYCONTROL = 0x0C
+    const LCD_CURSORSHIFT = 0x10
+    const LCD_FUNCTIONSET = 0x3C
+
+    // flags for display on/off control
+    const LCD_DISPLAYON = 0x04
+    const LCD_CURSORON = 0x02
+    const LCD_BLINKON = 0x01
+
 
     function command(cmd: number) {
         let buf = pins.createBuffer(2)
@@ -18,20 +29,35 @@ namespace grove_lcd {
         pins.i2cWriteBuffer(chipAdress, buf, false)
     }
 
-    //% block="Init display"
-    export function init() {
-        command(0b00111100);
-        command(0b00001100);
-        command(0b00000001);
-        command(0b00000111);
+    //% block="Blink On"
+    export function BlinkOn() {
+        command((LCD_DISPLAYCONTROL | LCD_BLINKON))
+        basic.pause(1)
     }
 
-    // //% block="text = $text"
-    // export function printText(text: string) {
-    //     for (let i = 0; i < text.length; i++) {
-    //         charPrint(text.charCodeAt(i))
-    //     }
-    // }
+    //% block="Blink Off"
+    export function BlinkOff() {
+        command((LCD_DISPLAYCONTROL &= ~LCD_BLINKON))
+        basic.pause(1)
+    }
+
+    //% block="Cursor On"
+    export function CursorOn() {
+        command((LCD_DISPLAYCONTROL | LCD_CURSORON))
+        basic.pause(1)
+    }
+
+    //% block="Cursor Off"
+    export function CursorOff() {
+        command((LCD_DISPLAYCONTROL &= ~LCD_CURSORON))
+        basic.pause(1)
+    }
+
+    //% block="Clear"
+    export function clear() {
+        command(LCD_CLEARDISPLAY)
+        basic.pause(2)
+    }
 
     //% block="text = $text on X = $x Y = $y"
     export function printTextXY(text: string, x: number, y: number) {
@@ -46,10 +72,20 @@ namespace grove_lcd {
         }
     }
 
-    //% block="Clear"
-    export function clear() {
-        command(LCD_CLEARDISPLAY)
-        basic.pause(2)
+    //% block="Init display"
+    export function init() {
+        command(LCD_FUNCTIONSET);       //Function set
+        command(LCD_DISPLAYCONTROL);    //On/Off control
+        command(LCD_CLEARDISPLAY);      //Clear
+        command(LCD_ENTRYMODESET);      //Entry mode set
+        basic.pause(1)
     }
+
+    // //% block="text = $text"
+    // export function printText(text: string) {
+    //     for (let i = 0; i < text.length; i++) {
+    //         charPrint(text.charCodeAt(i))
+    //     }
+    // }
 
 }
--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let index = 0
+grove_lcd.init()
+basic.forever(function () {
+    for (let index = 0; index <= 9; index++) {
+        grove_lcd.printTextXY("test!", index, 1)
+        basic.pause(1000)
+        grove_lcd.clear()
+        basic.pause(1000)
+    }
+})
